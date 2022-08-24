757195b7ddfeb84d8f48d7a44d9073d2b2ade073
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-08-24 15:18:53 +0200

    header modif

--- header.php
+++ header.php
@@ -7,7 +7,47 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+<?php wp_body_open();
+// Create a function who return a loop of li tags
+function get_contact_menu() {
+	$menu_items_first  = '';
+	$menu_items_second = '';
+	$menu_items_third  = '';
+	$menu_items_fourth = '';
+	$menu_items_last   = '';
+	if ( have_rows( 'panier_compte_contact', 'option' ) ) :
+		$menu_items_first = '<div class="header-infos"><nav class="commerce" id="commerce-links"><ul id="commerce-list">';
+
+		while ( have_rows( 'panier_compte_contact', 'options' ) ) : the_row();
+			$lien        = get_sub_field( 'lien' );
+			$lien_url    = $lien['url'];
+			$lien_title  = $lien['title'];
+			$lien_target = $lien['target'];
+
+			$image_commerce     = get_sub_field( 'image' );
+			$image_commerce_url = $image_commerce['url'];
+			$image_commerce_alt = $image_commerce['alt'];
+			$image_deux         = get_sub_field( 'image_deux' );
+
+			$radio = get_sub_field( 'deuxieme_image' );
+
+			$menu_items_second = '<li class="pos"><a href="' . esc_url(
+                    $lien_url ) . '" target="' . esc_attr(
+                    $lien_target
+                ) . '"title="' . esc_html( $lien_title ) . '"><img src="' . esc_url( $image_commerce_url ) . '" alt="' . esc_attr( $image_commerce_alt ) . '" class="commerce__img">';
+			if ( $radio === 'Oui' ) :
+				$menu_items_third = '<img src="' . esc_url( $image_deux['url'] ) . '"alt="' . esc_attr( $image_deux['alt'] ) .'" class="commercer__second-img">';
+			endif;
+
+			$menu_items_fourth = '</a></li>';
+		endwhile;
+		$menu_items_last = '</ul></nav></div>';
+	endif;
+
+	return $menu_items_first . $menu_items_second . $menu_items_third . $menu_items_fourth . $menu_items_last;
+}
+
+?>
 
 <header>
     <div class="container">
@@ -15,13 +55,14 @@
 		$logo_url   = $logo['url'];
 		$logo_alt   = $logo['alt'];
 
-        $logo_mobile = get_field('logo_mobile', 'option');
-        $logo_mobile_url = $logo_mobile['url'];
-        $logo_mobile_alt = $logo_mobile['alt'];
+		$logo_mobile     = get_field( 'logo_mobile', 'option' );
+		$logo_mobile_url = $logo_mobile['url'];
+		$logo_mobile_alt = $logo_mobile['alt'];
 		?>
-        <a href="<?= home_url( $path = '/', $scheme = 'https' ); ?>">
+        <a href="<?= home_url( $path = '/', $scheme = 'https' ); ?>" class="logo-link">
             <img src="<?= esc_url( $logo_url ) ?>" alt="<?= esc_attr( $logo_alt ) ?>" class="logo-desktop">
-            <img src="<?= esc_url($logo_mobile_url); ?>" alt="<?= esc_attr($logo_mobile_alt); ?>" class="logo-mobile">
+            <img src="<?= esc_url( $logo_mobile_url ); ?>" alt="<?= esc_attr( $logo_mobile_alt ); ?>"
+                 class="logo-mobile">
         </a>
         <div class="main-menu__container">
 			<?php wp_nav_menu( array(
@@ -33,38 +74,39 @@
                     <div class="menu-btn__burger">
                     </div>
                 </div><ul id="%1$s" class="%2$s">%3$s<div class="form-button__container">
-            </div></ul>',
+            </div>' . get_contact_menu() . '</ul>',
 			) ); ?>
         </div>
 		<?php if ( have_rows( 'panier_compte_contact', 'option' ) ) : ?>
             <div class="header-infos">
                 <nav class="commerce" id="commerce-links">
                     <ul id="commerce-list">
-                        <?php while( have_rows('panier_compte_contact', 'option') ) : the_row();
-                            $lien = get_sub_field('lien');
-                            $lien_url = $lien['url'];
-                            $lien_title = $lien['title'];
-                            $lien_target = $lien['target'];
+						<?php while ( have_rows( 'panier_compte_contact', 'option' ) ) : the_row();
+							$lien        = get_sub_field( 'lien' );
+							$lien_url    = $lien['url'];
+							$lien_title  = $lien['title'];
+							$lien_target = $lien['target'];
 
-                            $image_commerce = get_sub_field('image');
-                            $image_commerce_url = $image_commerce['url'];
-                            $image_commerce_alt = $image_commerce['alt'];
-                            $image_deux = get_sub_field('image_deux');
+							$image_commerce     = get_sub_field( 'image' );
+							$image_commerce_url = $image_commerce['url'];
+							$image_commerce_alt = $image_commerce['alt'];
+							$image_deux         = get_sub_field( 'image_deux' );
 
-                            $radio = get_sub_field('deuxieme_image');
-                        ?>
+							$radio = get_sub_field( 'deuxieme_image' );
+							?>
                             <li class="<?= $radio === 'Oui' ? 'pos' : '' ?>">
-                                <a href="<?= esc_url($lien_url) ?>" target="<?= esc_attr($lien_target); ?>"
-                                   title="<?= esc_html($lien_title); ?>" >
-                                    <img src="<?= esc_url($image_commerce_url); ?>" alt="<?= esc_attr
-                                    ($image_commerce_alt); ?>" class="commerce__img">
-                                    <?php if( $radio === 'Oui' ) : ?>
-                                        <img src="<?= esc_url($image_deux['url']); ?>" alt="<?= esc_attr($image_deux['alt']);
-                                        ?>" class="commercer__second-img">
-                                    <?php endif; ?>
+                                <a href="<?= esc_url( $lien_url ) ?>" target="<?= esc_attr( $lien_target ); ?>"
+                                   title="<?= esc_html( $lien_title ); ?>">
+                                    <img src="<?= esc_url( $image_commerce_url ); ?>" alt="<?= esc_attr
+									( $image_commerce_alt ); ?>" class="commerce__img">
+									<?php if ( $radio === 'Oui' ) : ?>
+                                        <img src="<?= esc_url( $image_deux['url'] ); ?>"
+                                             alt="<?= esc_attr( $image_deux['alt'] );
+										     ?>" class="commercer__second-img">
+									<?php endif; ?>
                                 </a>
                             </li>
-                        <?php endwhile; ?>
+						<?php endwhile; ?>
                     </ul>
                 </nav>
             </div>
