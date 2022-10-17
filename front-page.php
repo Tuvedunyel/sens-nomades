96c60ec6ea24318b2d2ae679712211f946d0c5c8
Tuvedunyel <mightygodx@gmail.com>
2022-10-17 16:45:36 +0200

    front page full

--- front-page.php
+++ front-page.php
@@ -9,6 +9,11 @@ class Resume_product {
 	public $thematique;
 	public $dates;
 	public $permalink;
+	public $longitude;
+	public $latitude;
+	public $price;
+	public $link;
+	public $days;
 }
 
 $resume_product_list = [];
@@ -40,6 +45,12 @@ if ( $query->have_posts() ) :
 					$resume_product->dates[] = get_sub_field( 'date' );
 				endwhile; endif;
 			$resume_product->permalink = get_permalink();
+			$resume_product->longitude = get_field( 'longitude' );
+			$resume_product->latitude  = get_field( 'latitude' );
+			$resume_product->price     = get_field( 'price' );
+			$resume_product->link      = get_the_permalink();
+			$resume_product->tags      = get_field( 'tag_image' );
+			$resume_product->days      = get_field( 'nombre_de_jours' );
 			$resume_product_list[]     = $resume_product;
 		endif;
 
@@ -93,6 +104,140 @@ json_encode( $resume_product_list );
             </div>
         </div>
     </section>
+    <section class="map">
+        <div class="link-to">
+			<?php the_field( 'texte_dessus_carte' ); ?>
+			<?php $lien_carte  = get_field( 'lien_vers_carte' );
+			$lien_carte_target = $lien_carte['target'] ? $lien_carte['target'] : '_self';
+			?>
+            <a href="<?= esc_url( $lien_carte['url'] ); ?>" target="<?= esc_attr( $lien_carte_target ); ?>" class="btn
+            btn-to-map"><?=
+				esc_html
+				( $lien_carte['title'] );
+				?></a>
+        </div>
+        <div id="map-container"></div>
+    </section>
+
+    <section class="privatiser-area">
+        <div class="container-narrow">
+            <div class="privatiser__content">
+                <strong><?php the_field( 'texte_privatiser' ); ?></strong>
+				<?php $lien_privatiser  = get_field( 'lien_privatiser' );
+				$lien_privatiser_target = $lien_privatiser['target'] ? $lien_privatiser['target'] : '_self';
+				?>
+                <a href="<?= esc_url( $lien_privatiser['url'] ); ?>" target="<?= esc_attr( $lien_carte_target ); ?>"><?=
+					esc_html
+					( $lien_privatiser['title'] );
+					?></a>
+            </div>
+            <div class="randonneur">
+                <img src="<?= get_template_directory_uri() ?>/assets/randonneur-black.svg" alt="Petit randonneur">
+                <div><?php the_field( 'texte_randonneur' ); ?></div>
+                <img src="<?= get_template_directory_uri() ?>/assets/fleche.svg" alt="Flèche en pointillet noir
+                pointant vers le haut">
+            </div>
+        </div>
+    </section>
+
+    <section class="directions" v-if="isLoad">
+        <div class="container-narrow">
+            <h2><?php the_field( 'titre_direction' ); ?></h2>
+			<?php if ( have_rows( 'directions' ) ) :
+				class Directions {
+					public $image;
+					public $titre;
+					public $texte;
+				}
+
+				$direction_array = [];
+				?>
+                <div class="directions__container">
+					<?php while ( have_rows( 'directions' ) ) : the_row();
+						$direction        = new Directions();
+						$direction->image = get_sub_field( 'image' );
+						$direction->titre = get_sub_field( 'titre' );
+						$direction->texte = get_sub_field( 'texte' );
+
+						$direction_array[] = $direction;
+					endwhile;
+					?>
+                    <div class="direction" v-for="(direction, index) in directions" :key="index">
+                        <img :src="direction.image.url" :alt="direction.image.alt" class="illustration">
+                        <h3>{{direction.titre}}</h3>
+                        <div class="open">
+                            <img src="<?= get_template_directory_uri() ?>/assets/open.svg" alt="Ouvrir le détail"
+                                 class="open" @click="toggleElement(index)" v-if="!directionsShow[index].show">
+                            <img src="<?= get_template_directory_uri() ?>/assets/close.svg" alt="Fermer le détail"
+                                 class="close" @click="toggleElement(index)" v-else>
+                        </div>
+                        <div class="texte-direction" v-if="directionsShow[index].show">
+                            <p>{{direction.texte}}</p>
+                        </div>
+                    </div>
+                </div>
+			<?php endif; ?>
+        </div>
+    </section>
+
+    <section class="actualites">
+        <div class="container-narrow">
+            <h2><?php the_field( 'titre_actualites' ); ?></h2>
+            <p><?php the_field( 'texte_actualites' ); ?></p>
+			<?php
+			$args = array(
+				'post_type'      => 'post',
+				'posts_per_page' => 2,
+				'post_status'    => 'publish',
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+			);
+
+			$query = new WP_Query( $args );
+
+			if ( $query->have_posts() ) :
+				?>
+                <div class="card-container">
+					<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                        <div class="articles__card">
+                            <div class="card__thumbnail">
+								<?php the_post_thumbnail(); ?>
+                            </div>
+                            <div class="card__content">
+                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <p><?php the_excerpt(); ?></p>
+                                <a href="<?php the_permalink(); ?>">Lire la suite</a>
+                            </div>
+                        </div>
+					<?php endwhile;
+					wp_reset_postdata(); ?>
+                </div>
+			<?php endif;
+            $lien_actu = get_field('page_darticles', 'option');
+            $lien_actu_target = $lien_actu['target'] ? $lien_actu['target'] : '_self';
+            ?>
+            <a href="<?= esc_url($lien_actu['url']); ?>" class="btn btn-actu" target="<?= esc_attr($lien_actu_target)
+            ; ?>" >Tous nos articles</a>
+        </div>
+    </section>
+
+    <?php get_template_part('parts/instagram'); ?>
+
+    <section class="avis">
+        <div class="container-narrow">
+            <h2><?php the_field('titre_avis'); ?></h2>
+            <?php if( have_rows('avis') ) : ?>
+                <div class="avis__container">
+                    <?php while( have_rows('avis') ) : the_row(); ?>
+                        <div class="avis-card">
+                            <?php $image = get_sub_field('image'); ?>
+                            <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>">
+                        </div>
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </section>
     <script>
 
         const { createApp } = Vue;
@@ -100,13 +245,67 @@ json_encode( $resume_product_list );
         createApp( {
             data () {
                 return {
-                    posts: null
+                    posts: null,
+                    map: null,
+                    products: null,
+                    markerIcon: L.icon( {
+                        iconUrl: '<?= get_stylesheet_directory_uri(); ?>/assets/marker.svg',
+                        iconSize: [ 38, 57 ],
+                        iconAnchor: [ 22, 44 ],
+                        popupAnchor: [ -3, -36 ]
+                    } ),
+                    directions: null,
+                    directionsShow: [],
+                    isLoad: false,
                 }
             },
             async mounted () {
                 await this.getPost();
+                await this.getDirections();
+                await this.setDirectionShow();
+                this.map = L.map( 'map-container', {
+                    attributionControl: false, dragging: false, zoomControl: false,
+                    boxZoom: false, doubleClickZoom: false, scrollWheelZoom: false, tap: false, touchZoom: false
+                } ).setView( [ 47.393, 2.739 ], 7 );
+                L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                } ).addTo( this.map );
+                this.setMapPoints();
+                this.isLoad = true
             },
             methods: {
+                toggleElement ( index ) {
+                    this.directionsShow[ index ].show = !this.directionsShow[ index ].show;
+                },
+                setDirectionShow () {
+                    let index = 0;
+                    this.directions.forEach( direction => {
+                        this.directionsShow.push( {
+                            index: index,
+                            show: false
+                        } );
+                        index++;
+                    } )
+                },
+                getDirections () {
+                    this.directions = <?= json_encode( $direction_array ); ?>;
+                },
+                setMapPoints () {
+                    this.posts.forEach( product => {
+                        const marker = L.marker( [ Number( product.longitude ).toFixed( 4 ), Number( product.latitude
+                        ).toFixed( 4 ) ], { icon: this.markerIcon } ).addTo( this.map );
+                        marker.bindPopup( `<div class="marker-map"><div class="upper">${ product
+                            .image }<div class="right-marker"><div class="fanion-container"><div
+                            class="days-spend">${ product
+                            .days }</div><div
+                            class="price-marker">${ product.price }€</div></div><div class="theme__container"><div
+                            class="image__picto"><img
+                            src="${ product.tags[ 'url' ] }"alt="${ product.tags[ 'alt' ] }" title="${ product.title }"
+                            /></div><p>${ product.thematique.replace( '- ', '' ) }</p></div></div></div><div
+                            class="lower"><h4>${ product.titre }</h4><a href="${ product.link }"
+                            target="_blank">Voir les dates</a></div></div>` );
+                    } );
+                },
                 getPost () {
                     this.posts = <?= json_encode( $resume_product_list ) ?>;
                 }
